ecc2510721ce0a4355b26288df2f5f759f07d2a1
Dhruv Desai <dde@narola.email>
2018-12-18 18:30:48 +0530

    Second commit

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Contact
 from .serializers import ContactSerializer
 from rest_framework.permissions import IsAdminUser
@@ -9,3 +11,10 @@ class ContactListAPIView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     #permission_classes = (IsAdminUser,)
+
+    # @action(methods=['get'], detail=False)
+    # def fetchContact(self, request):
+    #     acon = self.get_queryset()
+    #     serializer = self.get_serializer_class()(acon)
+    #     print(serializer.data)
+    #     return Response(serializer.data[0]['first_name'])
